7017d49d33a51a49c30a7c35ca22336b63842040
exelpratama13 <127178875+exelpratama13@users.noreply.github.com>
2025-04-16 15:43:14 +0700

    Update payment_repository_impl.go

--- internal/repository/payment_repository_impl.go
+++ internal/repository/payment_repository_impl.go
@@ -1,75 +1,29 @@
-package repository
+package usecase
 
 import (
 	"payment-options/internal/models"
-	"time"
+	"payment-options/internal/repository"
+	"sync"
 )
 
-type paymentRepo struct{}
-
-func NewPaymentRepo() PaymentRepository {
-	return &paymentRepo{}
+type paymentUsecase struct {
+	repo repository.PaymentRepository
 }
 
-func (r *paymentRepo) CallOVO() models.PaymentMethod {
-	time.Sleep(2 * time.Second) // Simulate network delay
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/ovo.jpg",
-	}
+func NewPaymentUsecase(r repository.PaymentRepository) PaymentUsecase {
+	return &paymentUsecase{repo: r}
 }
 
-func (r *paymentRepo) CallDANA() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/dana.jpg",
-	}
-}
+func (u *paymentUsecase) GetPaymentOptions() (map[string]models.PaymentMethod, error) {
+	result := make(map[string]models.PaymentMethod)
 
-func (r *paymentRepo) CallGoPay() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/gopay.jpg",
-	}
-}
+	result["ovo"] = u.repo.CallOVO()
+	result["dana"] = u.repo.CallDANA()
+	result["gopay"] = u.repo.CallGoPay()
+	result["shopee"] = u.repo.CallShopee()
+	result["oneklik"] = u.repo.CallOneKlik()
+	result["bridd"] = u.repo.CallBRIDD()
+	result["linkaja"] = u.repo.CallLinkAja()
 
-func (r *paymentRepo) CallShopee() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/shopee.jpg",
-	}
-}
-
-func (r *paymentRepo) CallOneKlik() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/oneklik.jpg",
-	}
-}
-
-func (r *paymentRepo) CallBRIDD() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/bridd.jpg",
-	}
-
-func (r *paymentRepo) CallLinkAja() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "0812xx", 
-		Status:  "Active", 
-		Balance: "150000", 
-		Icon:    "https://sampleurl.com/linkaja.jpg",
-	}
+	return result, nil
 }
