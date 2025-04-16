6cdd8e9edf1405dc46d1cd778c69a3ce0cd572af
exelpratama13 <127178875+exelpratama13@users.noreply.github.com>
2025-04-16 16:33:54 +0700

    Update payment_repository_impl.go

--- internal/repository/payment_repository_impl.go
+++ internal/repository/payment_repository_impl.go
@@ -2,7 +2,7 @@ package repository
 
 import (
 	"payment-options/internal/models"
-	//"time"
+	"time"
 )
 
 type paymentRepo struct{}
@@ -12,66 +12,74 @@ func NewPaymentRepo() PaymentRepository {
 }
 
 func (r *paymentRepo) CallOVO() models.PaymentMethod {
-// Simulate network delay
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/ovo.jpg",
-	}
+    time.Sleep(3 * time.Second)  // Add 2 second delay
+    return models.PaymentMethod{
+        Account: "6288xx",
+        Status:  "Active",
+        Balance: "10000",
+        Icon:    "https://sampleurl.com/ovo.jpg",
+    }
 }
 
 func (r *paymentRepo) CallDANA() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/dana.jpg",
-	}
+    time.Sleep(1 * time.Second)  // Add 1 second delay
+    return models.PaymentMethod{
+        Account: "6288xx",
+        Status:  "Active",
+        Balance: "10000",
+        Icon:    "https://sampleurl.com/dana.jpg",
+    }
 }
 
 func (r *paymentRepo) CallGoPay() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/gopay.jpg",
-	}
+    time.Sleep(1500 * time.Millisecond)  // Add 1.5 second delay
+    return models.PaymentMethod{
+        Account: "6288xx",
+        Status:  "Active",
+        Balance: "10000",
+        Icon:    "https://sampleurl.com/gopay.jpg",
+    }
 }
 
 func (r *paymentRepo) CallShopee() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/shopee.jpg",
-	}
+    time.Sleep(2 * time.Second)  // Add 2 second delay
+    return models.PaymentMethod{
+        Account: "6288xx",
+        Status:  "Active",
+        Balance: "10000",
+        Icon:    "https://sampleurl.com/shopee.jpg",
+    }
 }
 
 func (r *paymentRepo) CallOneKlik() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/oneklik.jpg",
-	}
+    time.Sleep(1 * time.Second)  // Add 1 second delay
+    return models.PaymentMethod{
+        Account: "6288xx",
+        Status:  "Active",
+        Balance: "10000",
+        Icon:    "https://sampleurl.com/oneklik.jpg",
+    }
 }
 
 func (r *paymentRepo) CallBRIDD() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "6288xx",
-		Status:  "Active",
-		Balance: "10000",
-		Icon:    "https://sampleurl.com/bridd.jpg",
-	}
+    time.Sleep(1500 * time.Millisecond)  // Add 1.5 second delay
+    return models.PaymentMethod{
+        Account: "6288xx",
+        Status:  "Active",
+        Balance: "10000",
+        Icon:    "https://sampleurl.com/bridd.jpg",
+    }
 }
+
 func (r *paymentRepo) CallLinkAja() models.PaymentMethod {
-	return models.PaymentMethod{
-		Account: "0812xx", 
-		Status:  "Active", 
-		Balance: "150000", 
-		Icon:    "https://sampleurl.com/linkaja.jpg",
-	}
+    time.Sleep(2 * time.Second)  // Add 2 second delay
+    return models.PaymentMethod{
+        Account: "0812xx", 
+        Status:  "Active", 
+        Balance: "150000", 
+        Icon:    "https://sampleurl.com/linkaja.jpg",
+    }
 }
+
 
 
